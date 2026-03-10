cf296e6000a7be4135f671166fdfa75afeb7ddc8
Aloysius <aloysius.tan00@gmail.com>
2026-03-10 23:14:09 +0800

    Added background image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,19 @@ export default function Navbar() {
     <nav>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" end className={({ isActive }) => isActive ? "active" : ""}>
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/characters">Characters</NavLink>
+          <NavLink to="/characters" end className={({ isActive }) => isActive ? "active" : ""}>
+            Characters
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/characters/yours">Your Characters</NavLink>
+          <NavLink to="/characters/yours" end className={({ isActive }) => isActive ? "active" : ""}>
+            Your Characters
+          </NavLink>
         </li>
       </ul>
     </nav>
